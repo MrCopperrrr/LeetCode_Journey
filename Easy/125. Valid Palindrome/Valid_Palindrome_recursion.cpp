e3f66338c79ad993cfe4426d027f0be08ea635c4
MrCopperrrr <ntphat0802@gmail.com>
2025-07-02 22:57:43 +0700

    Upload #125

---
+++ Easy/125. Valid Palindrome/Valid_Palindrome_recursion.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        return isPalindromeRec(s, 0, s.length() - 1);
+    }
+
+    bool isPalindromeRec(const string& s, int left, int right) {
+        while (left < right && !isalnum(s[left])) left++;
+        while (left < right && !isalnum(s[right])) right--;
+        
+        if (left >= right) return true;
+
+        if (tolower(s[left]) != tolower(s[right])) return false;
+
+        return isPalindromeRec(s, left + 1, right - 1);
+    }
+};
